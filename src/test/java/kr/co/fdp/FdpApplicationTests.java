b8e1826612cd66f94606663d92f62e7ba3698465
youspend8 <youspend8@gmail.com>
2019-07-14 01:15:18 +0900

    Initial Commit

---
+++ src/test/java/kr/co/fdp/FdpApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.fdp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FdpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
